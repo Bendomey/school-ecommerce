133949d0d3052cb112a503f45029b22318f93d50
Benjamin Domey <domeybenjamin1@gmail.com>
2021-01-12 18:14:13 +0000

    added product design

--- components/molecules/Collections/main.tsx
+++ components/molecules/Collections/main.tsx
@@ -2,12 +2,47 @@ import * as React from "react";
 import { Props } from "./types.d";
 import ProductComponent from "../ProductInGrid";
 
+const data = [
+  {
+    _id: 1,
+    name: "Night Party Dress",
+    price: 108,
+    discount: null,
+    stock: 10,
+    image: "http://deothemes.com/envato/aha/shop/img/shop/shop_item_1.jpg",
+  },
+  {
+    _id: 2,
+    name: "Night Party Dress",
+    price: 128,
+    discount: null,
+    stock: 0,
+    image: "http://deothemes.com/envato/aha/shop/img/shop/shop_item_2.jpg",
+  },
+  {
+    _id: 3,
+    name: "Night Party Dress",
+    price: 148,
+    discount: 10,
+    stock: 10,
+    image: "http://deothemes.com/envato/aha/shop/img/shop/shop_item_3.jpg",
+  },
+  {
+    _id: 4,
+    name: "Night Party Dress",
+    price: 18,
+    discount: null,
+    stock: 10,
+    image: "http://deothemes.com/envato/aha/shop/img/shop/shop_item_4.jpg",
+  },
+];
+
 const CollectionsComponent: React.FC<Props> = ({ name }) => {
   return (
     <React.Fragment>
       <div className={"py-12 sm:py-12 md:py-16"}>
         {/* Header */}
-        <div className={"w-full flex justify-center"}>
+        <div className={"w-full flex mb-10 justify-center"}>
           <span
             className={
               "text-center uppercase tracking-wider text-2xl font-light text-gray-600 dark:text-white"
@@ -22,10 +57,11 @@ const CollectionsComponent: React.FC<Props> = ({ name }) => {
             "mt-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3"
           }
         >
-          <ProductComponent />
-          <ProductComponent />
-          <ProductComponent />
-          <ProductComponent />
+          {data?.map((product: any, i: number) => (
+            <React.Fragment key={i}>
+              <ProductComponent data={product} />
+            </React.Fragment>
+          ))}
         </div>
       </div>
     </React.Fragment>
--- components/molecules/ProductInGrid/main.tsx
+++ components/molecules/ProductInGrid/main.tsx
@@ -1,23 +1,105 @@
 import * as React from "react";
+import { Props } from "./types.d";
 
-const ProductComponentInGrid = () => {
+const getDiscountPrice = (price: number, discount: number): string => {
+  let discountedPrice: number = price - (discount / 100) * price;
+
+  return discountedPrice.toFixed(2);
+};
+
+const ProductComponentInGrid: React.FC<Props> = ({ data }) => {
+  const [hoverEffect, setHoverEffect] = React.useState<boolean>(false);
   return (
     <React.Fragment>
       <div
         className={"w-full border-b border-gray-200 dark:border-gray-800 pb-5"}
       >
-        <div className={"bg-gray-50 dark:bg-gray-800  mb-3 w-full"}>
-          <img
-            src="http://deothemes.com/envato/aha/shop/img/shop/shop_item_3.jpg"
-            alt=""
-          />
+        <div
+          onMouseEnter={() => setHoverEffect(true)}
+          onMouseLeave={() => setHoverEffect(false)}
+          className={"bg-gray-50 dark:bg-gray-800  mb-3 w-full relative"}
+        >
+          <img src={data?.image} alt="" />
+          {/* for discounts */}
+          {data?.discount && (
+            <div className={"absolute top-4 right-4"}>
+              <div className={"bg-yellow-400 text-white p-1 text-sm"}>
+                {data?.discount}% DISCOUNT
+              </div>
+            </div>
+          )}
+          {/* for outta stock */}
+          {data?.stock === 0 && (
+            <div
+              className={
+                "absolute bottom-20 sm:bottom-20 md:bottom-32 w-full flex justify-center opacity-90 bg-white dark:bg-gray-900 py-3"
+              }
+            >
+              <span
+                className={
+                  "uppercase dark:text-white text-center text-xs font-extrabold"
+                }
+              >
+                out of stock
+              </span>
+            </div>
+          )}
+          {/* for viewing product */}
+          {hoverEffect && (
+            <React.Fragment>
+              {/* <div
+                className={
+                  "absolute  top-4 left-4 bg-white p-2 cursor-pointer group hover:bg-teal-500"
+                }
+              >
+                <span
+                  className={
+                    "uppercase tracking-wider text-center text-xs font-extrabold text-white flex justify-center"
+                  }
+                >
+                  <svg
+                    className={"h-5 w-5 text-gray-700 group-hover:text-white"}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </div> */}
+              <div
+                className={
+                  "absolute w-full bottom-0  bg-gray-700 py-3 cursor-pointer hover:bg-teal-500"
+                }
+              >
+                <span
+                  className={
+                    "uppercase tracking-wider text-center text-xs font-extrabold text-white flex justify-center"
+                  }
+                >
+                  quick view
+                </span>
+              </div>
+            </React.Fragment>
+          )}
         </div>
         <div className={"w-full flex flex-col items-center"}>
           <span className={"font-thin text-sm text-gray-400"}>
-            Night Party Dress
+            {data?.name}
           </span>
           <span className={"text-gray-900 mt-1 font-extrabold dark:text-white"}>
-            $159.99
+            {data?.discount && (
+              <React.Fragment>
+                <span className={"line-through text-gray-600 text-sm"}>
+                  GH¢ {data?.price}
+                </span>{" "}
+              </React.Fragment>
+            )}
+            GH¢ {getDiscountPrice(data?.price, data?.discount)}
           </span>
         </div>
       </div>
